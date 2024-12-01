5de3b2ee456007fb61ae5e54287846e7e9677c55
norihitoishida <norihitoishida@gmail.com>
2024-12-01 14:51:11 +0900

    3日目まで

--- src/demo.py
+++ src/demo.py
@@ -1,18 +1,31 @@
-from lexer import Lexer
-import queue
-import re
+"""import re
 
-#l = Lexer(source_code_path="stone/sample.stone")
-#print(l.temp_queue)
-
-blank = r"\s*" 
+blank = r"\s*"
 comment = "#.*"
 number_literal = "[0-9]+"
 string_literal = '".*"'
 identifier = r"[a-zA-Z_][a-zA-Z0-9_]*|==|<=|>=|&&|\|\||!|\"|$|%|&|'|\(|\)|\*|\+|,|-|\.|/|:|;|<|=|>|\?|@|\[|\\|\]|\^|_|`|{|\||}|~"
 
-pattern = re.compile(rf'{blank}(({comment})|({number_literal})|({string_literal})|({identifier}))?')
+comment_pattern = re.compile(rf"{blank}({comment})")
+number_literal_pattern = re.compile(rf"{blank}({number_literal})")
+string_literal_pattern = re.compile(rf"{blank}({string_literal})")
+identifier_pattern = re.compile(rf"{blank}({identifier})")
 
-s = "while i < 10 {"
-print(pattern.match(string=s))
+s = ""
+if comment_pattern.match(string=s):
+    print("comment_pattern")
+    print(comment_pattern.match(string=s).group(0))
+elif number_literal_pattern.match(string=s):
+    print("number_literal_pattern")
+    print(number_literal_pattern.match(string=s).group(0))
+elif string_literal_pattern.match(string=s):
+    print("string_literal_pattern")
+elif identifier_pattern.match(string=s):
+    print("identifier_pattern")
+    print(identifier_pattern.match(string=s).group(0))
+else:
+    print("naaaaaaa")
+"""
 
+s = "123 "
+print(int(s.strip()))
--- src/lexer.py
+++ src/lexer.py
@@ -1,30 +1,89 @@
 import re
 from stone.token.token import BaseToken, IdentifierToken, NumberToken, StringToken
 
+
 class Lexer:
     """字句解析器"""
 
     def __init__(self, source_code_path: str):
         self.source_code_path = source_code_path
-        self.pattern = self.regex_pattern_compile() # 正規表現パターン
-        self.temp_queue = self.read_source_code() # ソースコード(1行ずつ)
+        (
+            self.comment_pattern,
+            self.number_literal_pattern,
+            self.string_literal_pattern,
+            self.identifier_pattern,
+        ) = self.compile_regex_pattern()  # 正規表現パターン
+        self.temp_queue = self.read_source_code()  # ソースコード(1行ずつ)
         self.tokens_list = []
-    
+        self.read_tokens()
+
     def read_source_code(self) -> list:
-        """ソースコードを読んで1行ずつ`self.temp_queue`に格納する
-        """
+        """ソースコードを読んで1行ずつ`self.temp_queue`に格納する"""
         with open(self.source_code_path, "r", encoding="utf-8") as f:
             temp_queue = f.readlines()
         return temp_queue
 
-    def regex_pattern_compile(self):
-        _blank = r"\s*" 
+    def compile_regex_pattern(self):
+        _blank = r"\s*"
         _comment = "#.*"
         _number_literal = "[0-9]+"
         _string_literal = '".*"'
-        _identifier = r"[a-zA-Z_][a-zA-Z0-9_]*|==|<=|>=|&&|\|\||!|\"|$|%|&|'|\(|\)|\*|\+|,|-|\.|/|:|;|<|=|>|\?|@|\[|\\|\]|\^|_|`|{|\||}|~"
-        pattern = re.compile(rf'{_blank}(({_comment})|({_number_literal})|({_string_literal})|({_identifier}))?')
-        return pattern
+        _identifier = r"[a-zA-Z_][a-zA-Z0-9_]*|==|<=|>=|&&|\|\||!|\"|$|%|&|'|\(|\)|\*|\+|,|-|\.|/|:|;|<|=|>|\?|@|\[|\\|\]|\^|\_|`|{|\||}|~"
 
-    def read_tokens(self, lines: list[str, int]):
+        comment_pattern = re.compile(rf"{_blank}({_comment})")
+        number_literal_pattern = re.compile(rf"{_blank}({_number_literal})")
+        string_literal_pattern = re.compile(rf"{_blank}({_string_literal})")
+        identifier_pattern = re.compile(rf"{_blank}({_identifier})")
+
+        return (
+            comment_pattern,
+            number_literal_pattern,
+            string_literal_pattern,
+            identifier_pattern,
+        )
+
+    def read_tokens(self) -> None:
+        for _linenum, _line in enumerate(self.temp_queue):
+            while True:    
+                if self.comment_pattern.match(string=_line):
+                    break
+                elif _line=="\n":
+                    self.tokens_list.append(BaseToken.EOL)
+                    break
+                elif self.number_literal_pattern.match(string=_line):
+                    m = self.number_literal_pattern.match(string=_line)
+                    _value = int(str(m.group(0)).strip())
+                    self.tokens_list.append(NumberToken(line=_linenum, value=_value))
+                    _line = _line[len(str(m.group(0))):]
+                    if len(_line)==0:
+                        break
+                elif self.string_literal_pattern.match(string=_line):
+                    m = self.string_literal_pattern.match(string=_line)
+                    _text = str(m.group(0)).strip()
+                    self.tokens_list.append(StringToken(line=_linenum, text=_text))
+                    _line = _line[len(str(m.group(0))):]
+                    if len(_line)==0:
+                        break
+                elif self.identifier_pattern.match(string=_line):
+                    m = self.identifier_pattern.match(string=_line)
+                    _identifier = str(m.group(0)).strip()
+                    self.tokens_list.append(IdentifierToken(line=_linenum, identifier=_identifier))
+                    _line = _line[len(str(m.group(0))):]
+                    if len(_line)==0:
+                        break
+                else:
+                    if len(_line)==0:
+                        break
+                
+        self.tokens_list.append(BaseToken.EOF)
         return None
+
+
+lexer = Lexer(source_code_path="stone/sample.stone")
+for token in lexer.tokens_list:
+    if not isinstance(token, str):
+        print(token.get_text())
+    else:
+        print(token)
+
+
